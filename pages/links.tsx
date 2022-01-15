7d7d1a34fbd3cfbd3e654dc01f2ddb82db666568
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2022-01-16 00:55:55 +0800

    feat: ssg link posts

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,7 +1,32 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 
 import { Links, PageLinksHead, SemiCircle } from "components/PageLinks";
 import Footer from "components/Footer";
+import { addApolloState, initializeApollo } from "lib/apolloClient";
+import { Query, QueryLinkPostsArgs } from "lib/api";
+import { GET_LINK_POSTS } from "lib/queries/posts";
+
+type QueryData = {
+  linkPostsConnection: Query["linkPostsConnection"];
+  couponsConnection: Query["couponsConnection"];
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query<
+    QueryData["linkPostsConnection"],
+    QueryLinkPostsArgs
+  >({
+    query: GET_LINK_POSTS,
+    variables: { first: 12 },
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+    revalidate: 1,
+  });
+};
 
 const PageLinks: NextPage = () => {
   return (
